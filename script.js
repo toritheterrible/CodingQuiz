d76de29448cd1256a03235276da18f96ea19768b
Tori <torimpham@gmail.com>
2019-12-09 19:51:10 -0800

    html layout minor css and array javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: "What is the capital of Wisconsin?",
+      choices: ["Milwaukee", "Green Bay", "Madison", "Eau Claire"],
+      answer: "Madison"
+    },
+    {
+      title: "What is the capital of California?",
+      choices: ["Sacramento", "Los Angeles", "San Francisco", "San Diego"],
+      answer: "Sacramento"
+    },
+    {
+        title: "What is the capital of Utah?",
+        choices: ["Salt Lake City", "St. George", "Park City", "Ogden"],
+        answer: "Salt Lake City"
+      },
+      {
+        title: "What is the capital of Florida?",
+        choices: ["Orlando", "Miami", "Sarasota", "Tallahassee"],
+        answer: "Sacramento"
+      },
+      {
+        title: "What is the capital of Tennessee?",
+        choices: ["Chattanooga", "Nashville","Memphis", "Knoxville"],
+        answer: "Nashville"
+      },
+
+
+
+
+
+
+
+    ///etc.
+  ];
